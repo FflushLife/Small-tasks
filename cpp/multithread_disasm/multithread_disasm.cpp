999d4140f1924d61780b5056a741117debefa468
mike <mikelka150@gmail.com>
2020-05-22 04:15:53 +0300

    added primitives for research

--- cpp/multithread_disasm/multithread_disasm.cpp
+++ cpp/multithread_disasm/multithread_disasm.cpp
@@ -1,7 +1,12 @@
 #include <atomic>
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
 
 int main() {
-  std::atomic<int> at_int;
+  std::atomic<int> _at_int;
+  std::mutex _lock;
+  std::recursive_mutex _rlock;
+  std::condition_variable _cv;
   return 0;
 }
